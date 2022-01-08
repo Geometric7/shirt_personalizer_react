2703bb878b1422b42caed5e8e63821fa406f5261
Marcin Dzikowicz <mistamasta7@gmail.com>
2022-01-08 19:54:41 +0100

    Fix color pick

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,20 +1,26 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
+import PropTypes from 'prop-types';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import ProductImage from '../ProductImage/ProductImage';
+import shortid from 'shortid'
 
-const Product = props => {
+const Product = ({title, basePrice, colors, sizes, name }) => {
+
+const [currentColor, setCurrentColor] = useState(colors[0]);
+const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
+const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
   return (
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
-      </div>
+      <ProductImage name={name} title={title} currentColor={currentColor} />
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>{basePrice}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
@@ -29,9 +35,7 @@ const Product = props => {
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map(color => <li key={shortid()}><button type="button" onClick={() => setCurrentColor(color)} className={clsx(prepareColorClassName(color), currentColor === color && styles.active)} /></li>)}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -41,6 +45,14 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.string.isRequired,
+  colors: PropTypes.string.isRequired,
+  sizes: PropTypes.string.isRequired,
 };
 
 export default Product;
